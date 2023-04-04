6483c5400263c5df08795f06b403006359663129
theliteprogrammer <219037801@student.uj.ac.za>
2023-04-04 03:47:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -28,13 +28,14 @@ public class Main {
         if (n <= 1) {
             return n;
         }
-        long[] array = new long[n + 1];
-        array[0] = 0;
-        array[1] = 1;
-        for(int i = 2; i < array.length; i++) {
-            array[i] = array[i - 1] + array[i - 2];
+        int previousLastDigit = 0;
+        int currentLastDigit = 1;
+        for (int i = 2; i <= n; i++) {
+            int nextLastDigit = (previousLastDigit + currentLastDigit) % 10;
+            previousLastDigit = currentLastDigit;
+            currentLastDigit = nextLastDigit;
         }
-        return array[n] % 10;
+        return currentLastDigit;
     }
 
     public static void main(String[] args) {
